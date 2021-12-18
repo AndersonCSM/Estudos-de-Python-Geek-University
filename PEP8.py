2963b1a85b027ed92fa3a101cd1a8742c9375202
Anderson Carlos <96142983+AndersonCSM@users.noreply.github.com>
2021-12-18 11:24:36 -0300

    Create pep8.py

---
+++ PEP8.py
@@ -0,0 +1,12 @@
+'''
+PEP8 - Python Enhancement Proposal
+São propostas de melhorias para a linguagem Python
+The Zen of Python
+
+import this
+
+A idéia de PEP8 é que possamos escrever códigos Python
+de forma Pythônica (agradável).
+[1] - Utilizade Camel Case para nome de classes;
+
+'''
