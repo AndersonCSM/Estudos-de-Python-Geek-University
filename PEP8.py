1ded80ead65cca19c55909d3bf97caa3805b8098
Anderson Carlos <96142983+AndersonCSM@users.noreply.github.com>
2021-12-18 12:34:32 -0300

    alguns adds

--- PEP8.py
+++ PEP8.py
@@ -7,6 +7,34 @@ import this
 
 A idéia de PEP8 é que possamos escrever códigos Python
 de forma Pythônica (agradável).
-[1] - Utilizade Camel Case para nome de classes;
+[1] - Utilizar Camel Case para nome de Classes;
+class Calculadora:
+
+    pass
+
+
+class calculadora_cinetifica: ERRADO 
+    pass
+
+
+class CalculadoraCientifica:  Correto
+    pass
+
+
+[2] - Utilizar nomes em minúsculo, sperados por underline para funções ou variáveis;
+numero = 4
+
+numero_impar = 5
+
+def soma():
+    pass
+
+def soma_dois():
+    pass
+
+
+[3] - Utilize 4 espaços para identação!
+if 'a' in 'banana':
+    print('tem')
 
 '''
